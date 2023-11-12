ff1f38c483ff3edf5b75e48df8eedf956d3653d1
Adisirinli <islamea@code.edu.az>
2023-11-12 14:44:25 +0400

    Add project files.

---
+++ lab4/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using lab4.models;
+
+person person1 = new person();
+person1.Name = "nazim";
+person1.Age = 30;
+person1.DisplayInfo();
---
+++ lab4/models/person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace lab4.models
+{
+    internal class person
+    {
+        private string name;
+        private int age;
+
+        public string Name 
+        { 
+            get 
+            { 
+                return name; 
+
+            } 
+
+            set    
+            {
+                name = value;
+            } 
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("yas sefdir.");
+                }
+            }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+   
+}
